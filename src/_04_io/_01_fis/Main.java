6cb56810000901c4e7aca5e8829f420eddb38c39
Ilston <Test5User@mail.ru>
2024-10-18 21:11:16 +0300

    Init

--- src/_04_io/_01_fis/Main.java
+++ src/_04_io/_01_fis/Main.java
@@ -1,5 +1,28 @@
 package _04_io._01_fis;
 
-public class Main {
+import lombok.SneakyThrows;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+    public static final String FILE1 = "src/_04_io/_01_fis/test";
+    public static final String FILE2 = "src/_04_io/_01_fis/test2";
+
+    @SneakyThrows
+    public static void main(String[] args) throws FileNotFoundException {
+        try (var fos = new FileOutputStream(FILE1);
+             var fis = new FileInputStream(FILE1);
+             var fos1 = new FileOutputStream(FILE2);
+        ) {
+            var outBytrArray = "Hollo мир!!!!".getBytes();
+            fos.write(outBytrArray);
+            byte[] bytes = fis.readAllBytes();
+            System.out.println(new String(bytes));
+
+            fos1.write(bytes);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
